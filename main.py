f16479f7e33983a91afa25374d4053a1975db317
Aleksy37 <aleksyveillette@gmail.com>
2025-01-20 22:06:22 -0500

    added asteroids and gun

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -24,6 +35,11 @@ def main():
             
         for entity in updatable:
             entity.update(dt)
+        
+        for entity in asteroids:
+            if entity.collision(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
